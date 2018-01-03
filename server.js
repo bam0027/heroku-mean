636e28d34a63d060ec3d4e0c3d1e170bba1fe123
Burt Lo <blo@blomain.net>
2018-01-02 16:40:21 -0800

    added local mongodb support

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.use(bodyParser.json());
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
+// mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/myLocalDb', function (err, database) {
+if (err) {
     console.log(err);
     process.exit(1);
   }
